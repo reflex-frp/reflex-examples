491243fcad383f786c0e095e87433e1a9a2cf18f
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2015-07-15 17:07:18 -0400

    clean up code a bit

--- othello/README.md
+++ othello/README.md
@@ -5,5 +5,7 @@ Reflex example of Othello
 ---------------------------------
 To do
 -----
+- [ ] Polish UI.
 - [ ] Legal squares change color when hovered over.
 - [ ] Show number of squares each player has.
+- [ ] Add control over depth of AI.
--- othello/src/Othello.hs
+++ othello/src/Othello.hs
@@ -20,21 +20,26 @@ import           Reflex.Dom
 -------------------------------------------------------------------------------
 main :: IO ()
 main = mainWidget $ do
-  elAttr "div" (fromList [("style", s)]) (text "Othello")
+  elAttr "div" ("style" =: s) $ text "Othello"
   setup
   where
     s = "font-size: 50px; margin-left: 155px; font-family: Helvetica; color: steelblue"
-  
+
+setup :: (MonadWidget t m) => m ()
+setup = el "div" $ do
+  rec rows <- mapM (row g) [1..8]
+      let bm = leftmost (concat rows)
+      wm <- performEventAsync $ fmap computeAiMove
+                              . ffilter ((==White) . player)
+                              $ attachWith (flip mkMove) (current g) bm
+      dynText =<< mapDyn ((++"'s move") . show . player) g
+      g <- foldDyn mkMove newGame (leftmost [wm, bm])
+  return ()
+
 -- | A button whose color is based on the game state.
 buttonDynAttr :: MonadWidget t m => Dynamic t (Map String String) -> m (Event t ())
 buttonDynAttr attrs = do
   (e, _) <- elDynAttr' "button" attrs (text "")
-  return $ domEvent Click e
-
--- | A button with attributes.
-buttonAttr :: MonadWidget t m => String -> Map String String -> m (Event t ())
-buttonAttr s attrs = do
-  (e, _) <- elAttr' "button" attrs (text s)
   return $ domEvent Click e
 
 -- | A button widget representing a square on the othello board.
@@ -63,20 +68,11 @@ row g n = el "div" $
 mkMove :: Input -> Game -> Game
 mkMove (BlackMove x) g@(Game Black _) = move x g
 mkMove (WhiteMove g) _ = g -- We trust that the AI's moves will always be legal
-mkMove _ g                            = g
+mkMove _ g = g
 
 computeAiMove :: MonadIO m => Game -> (Input -> IO ()) -> m ()
 computeAiMove g cb = liftIO $ do
-  forkIO $ do
---    threadDelay 1000000 -- Add a delay here if necessary
-    cb $!! WhiteMove $ aiMove 4 White g -- Fully evaluate before triggering the callback
-  return ()
-
-setup :: (MonadWidget t m) => m ()
-setup = el "div" $ do
-  rec rows <- mapM (row g) [1..8]
-      let bm = leftmost (concat rows)
-      wm <- performEventAsync $ fmap computeAiMove $ ffilter ((==White) . player) $ attachWith (flip mkMove) (current g) bm
-      dynText =<< mapDyn ((++"'s move") . show . player) g
-      g <- foldDyn mkMove newGame (leftmost [wm, bm])
+  _ <- forkIO $ do
+    -- threadDelay 1000000 -- Add a delay here if necessary
+    cb $!! WhiteMove $ aiMove 2 White g -- Fully evaluate before triggering the callback
   return ()

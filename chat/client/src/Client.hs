f02d8e878ce46743433609fff046d45ff9640862
Ali Abrar <aliabrar@gmail.com>
2015-12-01 00:06:47 -0500

    Use popup to add DMs and Channels

--- chat/client/src/Client.hs
+++ chat/client/src/Client.hs
@@ -47,7 +47,7 @@ bodyTag = divClass "flex-container" $ do
                                ]
             wsDown <- openWebSocket wsUp
             let newMsg = fmapMaybe (\x -> case x of Just (Down_Message e) -> Just e; _ -> Nothing) wsDown
-                newDm = attachWithMaybe (\isNew m -> if isNew m then Just m else Nothing) (dmIsNew <$> current nick <*> current chats) newMsg
+                newDm = attachWithMaybe (\isNew m -> if isNew m then Just (_message_from $ _envelope_contents m) else Nothing) (dmIsNew <$> current nick <*> current chats) newMsg
             messages <- foldDyn (\n ms -> reverse $ n : reverse ms) [] newMsg
             sendMsgs <- listWithKey chats $ \k c -> do
               relevantMessages <- mapDyn (filter (\m -> Just True == (relevantMessage <$> k <*> pure m))) messages
@@ -63,21 +63,17 @@ data Chat = Chat_DirectMessage Nick
 
 chatSettings
   :: MonadWidget t m
-  => Event t (Envelope Message) -- ^ New DM
+  => Event t Nick -- ^ New DM
   -> m ( Dynamic t (Maybe Nick) -- Current Nick
        , Event t ChannelId -- New Channel event
        , Dynamic t (Map (Maybe Chat) Chat) -- Connected Chats
        , Dynamic t (Maybe Chat) -- Currently selected Chat
        )
-chatSettings newDm = do
+chatSettings newDm = divClass "chat-settings" $ do
   n <- el "div" nickInput
-  rec c <- el "div" addChannel
-      cs <- foldDyn (\x -> Map.insert (Just $ Chat_Channel x) (Chat_Channel x)) Map.empty c
-      cClick <- listGroup cs cSelection chatListItem
+  rec (c, cs, cClick) <- chatSettingsGroup "CHANNELS" addChannel never Chat_Channel cSelection
       cSelection <- holdDyn Nothing $ leftmost [cClick, Nothing <$ dmClick]
-      dm <- el "div" addDirectMessage
-      dms <- foldDyn (\x -> Map.insert (Just $ Chat_DirectMessage x) (Chat_DirectMessage x)) Map.empty $ leftmost [dm, fmap (_message_from . _envelope_contents) newDm]
-      dmClick <- listGroup dms dmSelection chatListItem
+      (dm, dms, dmClick) <- chatSettingsGroup "DIRECT MESSAGES" addDirectMessage newDm Chat_DirectMessage dmSelection
       dmSelection <- holdDyn Nothing $ leftmost [dmClick, Nothing <$ cClick]
       chats <- combineDyn Map.union dms cs
       chat <- combineDyn (\r c -> case (r, c) of
@@ -85,6 +81,23 @@ chatSettings newDm = do
         (_, Just c') -> c
         _ -> Nothing) dmSelection cSelection
   return (n, c, chats, chat)
+  where
+    chatSettingsGroup heading addItem newItem tyCon sel = do
+      rec item <- chatSettingsHeading heading items addItem
+          items <- foldDyn (\x -> Map.insert (Just $ tyCon x) (tyCon x)) Map.empty $ leftmost [item, newItem]
+          click <- listGroup items sel chatListItem
+      return (item, items, click)
+    chatSettingsHeading label xs child = divClass "nav-group-heading" $ do
+      elClass "span" "nav-group-heading-label" $ text label
+      text " ("
+      display =<< mapDyn Map.size xs
+      text ")"
+      add <- liftM (domEvent Click) $ iconDyn (constDyn "plus-circle pull-right fa-fw nav-group-add")
+      rec newItem <- liftM (switch . current) $ widgetHold (return never) $
+            leftmost [ fmap (\_ -> popup child) add
+                     , fmap (\_ -> return never) newItem
+                     ]
+      return $ fmapMaybe id newItem
 
 chatListItem :: MonadWidget t m => Dynamic t Chat -> m ()
 chatListItem c = do
@@ -126,14 +139,14 @@ nickInput = do
   elDynAttr "small" nickMsgAttr $ dynText nickMsg
   return nick
 
-addDirectMessage :: MonadWidget t m => m (Event t Nick)
-addDirectMessage = do
-  addDM <- inputGroupWithButton ComponentSize_Small "Add DM" never $ icon "plus"
+addDirectMessage :: MonadWidget t m => Event t () -> m (Event t Nick)
+addDirectMessage focus = do
+  addDM <- inputGroupWithButton ComponentSize_Small "Add DM" focus $ icon "plus"
   return $ fmapMaybe (validNick . T.pack) addDM
 
-addChannel :: MonadWidget t m => m (Event t ChannelId)
-addChannel = do
-  addC <- inputGroupWithButton ComponentSize_Small "Add Channel" never $ icon "plus"
+addChannel :: MonadWidget t m => Event t () -> m (Event t ChannelId)
+addChannel focus = do
+  addC <- inputGroupWithButton ComponentSize_Small "Add Channel" focus $ icon "plus"
   return $ fmapMaybe (fmap ChannelId . validateNonBlank . T.pack) addC
 
 validNick :: Text -> Maybe Nick
@@ -144,9 +157,9 @@ displayMessage em = do
   t <- mapDyn _envelope_time em
   m <- mapDyn _envelope_contents em
   let timestampFormat = formatTime defaultTimeLocale "%r"
-  elAttr "span" ("style" =: "color: lightgray; font-family: monospace;") $ dynText =<< mapDyn (\x -> "(" <> timestampFormat x <> ") ") t
-  elAttr "span" ("style" =: "color: red;") $ dynText =<< mapDyn ((<>": ") . T.unpack . unNick . _message_from) m
-  el "span" $ dyn =<< mapDyn (mapM displayMessageElement . messageElements . _message_body) m
+  elClass "span" "message-timestamp" $ dynText =<< mapDyn (\x -> "(" <> timestampFormat x <> ") ") t
+  elClass "span" "message-from" $ dynText =<< mapDyn ((<>": ") . T.unpack . unNick . _message_from) m
+  elClass "span" "message-body" $ dyn =<< mapDyn (mapM displayMessageElement . messageElements . _message_body) m
   return ()
 
 data MessageElement = MessageElement_Text Text
@@ -213,12 +226,12 @@ buttonSize c = case c of
   ComponentSize_Large -> "btn-lg"
  
 icon :: MonadWidget t m => String -> m ()
-icon = iconDyn . constDyn
+icon = void . iconDyn . constDyn
 
-iconDyn :: MonadWidget t m => Dynamic t String -> m ()
+iconDyn :: MonadWidget t m => Dynamic t String -> m (El t)
 iconDyn k = do
   i <- mapDyn (\x -> "class" =: ("fa fa-" <> x)) k
-  elDynAttr "i" i $ return ()
+  liftM fst $ elDynAttr' "i" i $ return ()
 
 buttonClass :: MonadWidget t m => String -> m a -> m (Event t ())
 buttonClass klass child = liftM (domEvent Click . fst) $ elAttr' "button" ("type" =: "button" <> "class" =: klass) child
@@ -240,6 +253,15 @@ listGroup :: (Ord k, MonadWidget t m) => Dynamic t (Map k v) -> Dynamic t k -> (
 listGroup as selection child = divClass "list-group" $ selectViewListWithKey_ selection as $ \_ v s -> do
   style <- forDyn s $ \active -> "style" =: "cursor: pointer;" <> "class" =: ("list-group-item" <> if active then " active" else "")
   liftM (domEvent Click . fst) $ elDynAttr' "a" style $ child v --dynText =<< mapDyn toString v
+
+popup :: MonadWidget t m => (Event t () -> m (Event t a)) -> m (Event t (Maybe a))
+popup child = do
+  elAttr "div" ("style" =: "position: relative;") $ divClass "dropdown-menu nav-group-popup" $ do
+    close <- liftM (domEvent Click . fst) $ elAttr' "span" ("class" =: "fa-stack popup-close") $ do
+      icon "circle fa-stack-1x popup-close-bg"
+      icon "times-circle fa-stack-1x popup-close-fg"
+    c <- child =<< getPostBuild
+    return $ leftmost [fmap Just c, Nothing <$ close]
 
 openWebSocket :: MonadWidget t m => Event t [Up] -> m (Event t (Maybe Down))
 openWebSocket wsUp = do
@@ -287,19 +309,66 @@ customCss = [r|
       |] <> displayFlex <> [r|
     }
     .flex-nav { |] <>
-      flex 1 1 "20%" <>
+      flex 1 1 "220px" <>
       order 1 <> [r|
       background-color: #4D394B;
+      min-width: 220px;
     }
     .flex-content { |] <>
       displayFlex <>
-      flex 1 1 "80%" <>
+      flex 1 1 "100%" <>
       order 2 <> [r|
       flex-direction: column;
       -webkit-flex-direction: column;
       margin-top: auto;
       overflow: hidden;
       height: 100vh;
+    }
+    .chat-settings {
+      height: 100vh;
+      overflow-y: auto;
+      overflow-x: hidden;
+    }
+    .message-timestamp {
+      color: lightgray;
+      font-family: monospace;
+    }
+    .message-from {
+      color: red;
+      font-weight: bold;
+    }
+    .message-body {
+    }
+    .nav-group-heading {
+      color: #ab9ba9;
+      padding-left: 15px;
+    }
+    .nav-group-heading-label {
+      font-weight: bold;
+    }
+    .nav-group-add {
+      padding-right: 20px;
+      cursor: pointer;
+    }
+    .popup-close {
+      cursor: pointer;
+      position: absolute;
+      right: -20px;
+      top: -20px;
+      font-size: 150%;
+      z-index: 3;
+    }
+    .popup-close-bg {
+      color: white;
+      font-size: 110%;
+    }
+    .popup-close-fg {
+      color: #4D394B;
+    }
+    .nav-group-popup {
+      display: block;
+      margin-right: 10px;
+      box-shadow: 6px 12px 24px rgba(0,0,0,0.75);
     }
   |]
   where

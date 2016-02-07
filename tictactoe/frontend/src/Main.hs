92c9b476aa0f8238d07a8cee53a5473b0b807a75
Ali Abrar <aliabrar@gmail.com>
2016-02-07 16:33:12 -0500

    drawing a grid

--- tictactoe/frontend/src/Main.hs
+++ tictactoe/frontend/src/Main.hs
@@ -1,4 +1,20 @@
+import Control.Monad (replicateM, replicateM_)
 import Reflex.Dom
 
 main :: IO ()
-main = mainWidget $ text "Test"
+main = mainWidget $ do
+  el "h1" $ text "Tic Tac Toe"
+  tictactoe
+
+tictactoe :: MonadWidget t m => m ()
+tictactoe = el "table" $ do
+  replicateM_ 3 $
+    el "tr" $ replicateM 3 $
+      elAttr "td" ("style" =: "border: 1px solid black;") $ return ()
+
+data Marker = Marker_X
+            | Marker_O
+            deriving (Show, Read, Eq, Ord)
+
+-- inputWidget :: MonadWidget t m => m (Dynamic t (Maybe Marker))
+-- inputWidget =

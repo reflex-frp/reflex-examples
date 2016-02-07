02e6f85ed54d37629079dc175e648df1c409a322
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-02-07 17:24:02 -0500

    Add CSS for table

--- tictactoe/frontend/src/Main.hs
+++ tictactoe/frontend/src/Main.hs
@@ -1,16 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Control.Monad (replicateM, replicateM_)
 import Reflex.Dom
+import Data.ByteString (ByteString)
 
 main :: IO ()
-main = mainWidget $ do
+main = mainWidgetWithCss css $ do
   el "h1" $ text "Tic Tac Toe"
   tictactoe
+
+css :: ByteString
+css = "table { border-collapse: collapse; }\
+      \td + td, th + th { border-left: 1px solid; }\
+      \tr + tr { border-top: 1px solid; }"
 
 tictactoe :: MonadWidget t m => m ()
 tictactoe = el "table" $ do
   replicateM_ 3 $
     el "tr" $ replicateM 3 $
-      elAttr "td" ("style" =: "border: 1px solid black;") $ return ()
+      el "td" $ return ()
 
 data Marker = Marker_X
             | Marker_O

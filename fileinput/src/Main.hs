69417f795077b116316dd1463a0bf25d051dafb6
Cale Gibbard <cgibbard@gmail.com>
2016-03-05 23:14:04 -0500

    Update FileReader example for new ghcjs.

--- fileinput/src/Main.hs
+++ fileinput/src/Main.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE RecursiveDo #-}
+
 import Reflex.Dom
+import Reflex.Host.Class
+import Data.Dependent.Sum
+import Data.List
 import Data.Monoid
 import Data.Maybe
+import Control.Monad.Identity
 import Control.Monad.Trans
-import GHCJS.Types
-import GHCJS.Foreign
 import GHCJS.Marshal
+import GHCJS.Types
+import GHCJS.DOM.FileReader
 import GHCJS.DOM.Types hiding (Event)
 import GHCJS.DOM.EventM
 
@@ -13,9 +18,9 @@ main :: IO ()
 main = mainWidget $ do
   header
   filesDyn <- value <$> fileInput def
-  urlE <- dataURLFileReader (fmapMaybe listToMaybe (updated filesDyn))
+  urlE <- fmap (ffilter ("data:image" `isPrefixOf`)) . dataURLFileReader . fmapMaybe listToMaybe . updated $ filesDyn
   el "div" . widgetHold blank . ffor urlE $ \url ->
-    elAttr "img" ("src" =: url) blank
+    elAttr "img" ("src" =: url <> "style" =: "max-width: 80%") blank
   footer
 
 linkNewTab :: MonadWidget t m => String -> String -> m ()
@@ -25,7 +30,7 @@ linkNewTab href s =
 header :: MonadWidget t m => m ()
 header = do
   el "strong" $ do
-    linkNewTab "https://github.com/ryantrinkle/reflex-dom" "Reflex.Dom"
+    linkNewTab "https://github.com/reflex-frp/reflex-dom" "Reflex.Dom"
     text " FileInput test page"
   el "p" $ do
     text "Select an image file."
@@ -38,55 +43,28 @@ footer = do
     linkNewTab "https://github.com/reflex-frp/reflex-examples" "Reflex Examples"
     text " repo."
 
----- Implementation of FileReader stuff, pending an update of reflex-dom ----
----------------- to using a newer version of ghcjs-dom ----------------------
-
-newtype FileReader = FileReader { unFileReader :: JSRef FileReader }
-
-instance ToJSRef FileReader where
-  toJSRef (FileReader r) = return r
-  {-# INLINE toJSRef #-}
-
-instance FromJSRef FileReader where
-  fromJSRef = return . fmap FileReader . maybeJSNull
-  {-# INLINE fromJSRef #-}
-
-instance GObjectClass FileReader where
-  toGObject = GObject . castRef . unFileReader
-  unsafeCastGObject = FileReader . castRef . unGObject
-
-foreign import javascript unsafe "new FileReader()"
-        js_newFileReader :: IO FileReader
-
-newFileReader :: MonadIO m => m FileReader
-newFileReader = liftIO js_newFileReader
-
-foreign import javascript unsafe "$1[\"result\"]"
-        js_getResult :: FileReader -> IO JSString
-
-getResult :: MonadIO m => FileReader -> m String
-getResult fr = liftIO (fmap fromJSString (js_getResult fr))
-
-foreign import javascript unsafe "$1[\"readAsDataURL\"]($2)"
-        js_readAsDataURL :: FileReader -> File -> IO ()
-
-readAsDataURL :: (MonadIO m) => FileReader -> File -> m ()
-readAsDataURL fr f = liftIO (js_readAsDataURL fr f)
+askPostEvent :: MonadWidget t m => m (EventTrigger t a -> a -> IO ())
+askPostEvent = do
+  postGui <- askPostGui
+  runWithActions <- askRunWithActions
+  return (\t a -> postGui $ runWithActions [t :=> Identity a])
 
 buildEvent :: (MonadWidget t m)
-           => ((a -> IO ()) -> WidgetHost m r)
+           => ((a -> IO ()) -> IO (IO ()))
            -> m (Event t a)
-buildEvent install =
-  do pb <- getPostBuild
-     performEventAsync . (<$ pb) $ \k ->
-       install k >> return ()
+buildEvent install = do
+  postEvent <- askPostEvent
+  newEventWithTrigger (install . postEvent)
 
 dataURLFileReader :: (MonadWidget t m) => Event t File -> m (Event t String)
 dataURLFileReader request =
   do fileReader <- liftIO newFileReader
-     let handler :: (String -> IO ()) -> EventM UIEvent FileReader ()
-         handler k = liftIO $ k =<< getResult fileReader
-     performEvent_ (fmap (\f -> readAsDataURL fileReader f) request)
-     buildEvent (liftIO . connect "load" fileReader . handler)
-
--------------------------------------------------------------------------------
+     let getResultString :: FileReader -> IO (Maybe String)
+         getResultString fr = do v <- getResult fr
+                                 s <- fromJSVal v
+                                 return (fmap fromJSString s)
+         handler :: (Maybe String -> IO ()) -> EventM FileReader UIEvent ()
+         handler k = liftIO $ k =<< getResultString fileReader
+     performEvent_ (fmap (\f -> readAsDataURL fileReader (Just f)) request)
+     e <- buildEvent (on fileReader load . handler)
+     return (fmapMaybe id e)

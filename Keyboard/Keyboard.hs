4dde70317f421b86f8cbc40a15173321b2943383
paolino <paolo.veronelli@gmail.com>
2016-04-20 08:15:43 +0200

    fixed cast htmlarea error

--- Keyboard/Keyboard.hs
+++ Keyboard/Keyboard.hs
@@ -11,7 +11,7 @@ import Control.Monad.Trans
 import Control.Lens ((^.))
 import qualified Data.List.NonEmpty
 import GHCJS.DOM.Element (focus)
-import GHCJS.DOM.HTMLTextAreaElement  (castToHTMLTextAreaElement ,getSelectionStart)
+import GHCJS.DOM.HTMLInputElement  (getSelectionStart,setSelectionStart)
 import GHCJS.DOM.Types (HTMLInputElement)
 
 insertAt :: Int -> a -> [a] -> [a]
@@ -25,20 +25,18 @@ performArg f x = performEvent (fmap (liftIO . f) x)
 
 
 -- inputW  :: forall m t .MonadWidget t m => Event t Char -> m ()
-inputW track buttonE = do
-    rec let newStringE = attachWith (\v (c,n) -> insertAt n c v) cur posCharE
+inputW buttonE = do
+    rec let newStringE = attachWith (\v (c,n) -> (n + 1,insertAt n c v)) cur posCharE
             cur = current $ input ^. textInput_value  -- actual string 
             element = input ^. textInput_element --html element
-        input <- textInput $ def & setValue .~ newStringE 
-        posCharE <- performArg (track element) buttonE
+        input <- textInput $ def & setValue .~ fmap snd newStringE 
+        performArg ( setSelectionStart element) $ fmap fst newStringE
+        posCharE <- performArg (\c -> ((,) c) <$> getSelectionStart element) buttonE
     void $ performArg (const $ focus element) buttonE -- keep the focus right
 
 keys :: MonadWidget t m => m [Event t Char]
 keys = forM "qwerty" $ \c -> fmap (const c) <$> button [c]
 
-broken,working :: HTMLInputElement -> Char -> IO (Char,Int)
-broken e c = ((,) c) <$> getSelectionStart (castToHTMLTextAreaElement e)
-working e c =  return (c,100) 
 
-main = mainWidget $ el "div" $ elClass "div" "keys" keys >>= inputW broken . fromListE
+main = mainWidget $ el "div" $ elClass "div" "keys" keys >>= inputW . fromListE
 

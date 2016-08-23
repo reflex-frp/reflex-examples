1b348a8e85bf07e0378c073eace911ce24e268bd
Cale Gibbard <cgibbard@gmail.com>
2016-08-23 14:19:37 -0400

    Fix fileinput example, clarify it, and make it work with the new reflex-dom.

--- fileinput/src/Main.hs
+++ fileinput/src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE RecursiveDo, TypeFamilies, FlexibleContexts, OverloadedStrings #-}
 
 import Reflex.Dom
 import Reflex.Host.Class
@@ -6,24 +6,26 @@ import Data.Dependent.Sum
 import Data.List
 import Data.Monoid
 import Data.Maybe
+import Data.Text (Text)
+import qualified Data.Text as T
 import Control.Monad.Identity
 import Control.Monad.Trans
 import GHCJS.Marshal
 import GHCJS.Types
 import GHCJS.DOM.FileReader
-import GHCJS.DOM.Types hiding (Event)
+import GHCJS.DOM.Types (File, UIEvent)
 import GHCJS.DOM.EventM
 
 main :: IO ()
 main = mainWidget $ do
   header
   filesDyn <- value <$> fileInput def
-  urlE <- fmap (ffilter ("data:image" `isPrefixOf`)) . dataURLFileReader . fmapMaybe listToMaybe . updated $ filesDyn
+  urlE <- fmap (ffilter ("data:image" `T.isPrefixOf`)) . dataURLFileReader . fmapMaybe listToMaybe . updated $ filesDyn
   el "div" . widgetHold blank . ffor urlE $ \url ->
     elAttr "img" ("src" =: url <> "style" =: "max-width: 80%") blank
   footer
 
-linkNewTab :: MonadWidget t m => String -> String -> m ()
+linkNewTab :: MonadWidget t m => Text -> Text -> m ()
 linkNewTab href s =
   elAttr "a" ("href" =: href <> "target" =: "_blank") $ text s
 
@@ -43,37 +45,12 @@ footer = do
     linkNewTab "https://github.com/reflex-frp/reflex-examples" "Reflex Examples"
     text " repo."
 
-askPostEvent :: MonadWidget t m => m (EventTrigger t a -> a -> IO ())
-askPostEvent = do
-  postGui <- askPostGui
-  runWithActions <- askRunWithActions
-  return (\t a -> postGui $ runWithActions [t :=> Identity a])
-
-
-askPostEvent :: MonadWidget t m => m (EventTrigger t a -> a -> IO ())
-askPostEvent = do
-  postGui <- askPostGui
-  runWithActions <- askRunWithActions
-  return (\t a -> postGui $ runWithActions [t :=> a])
-
-
-buildEvent :: (MonadWidget t m)
-           => ((a -> IO ()) -> IO (IO ()))
-           -> m (Event t a)
-buildEvent install = do
-  postEvent <- askPostEvent
-  newEventWithTrigger (install . postEvent)
-
-dataURLFileReader :: (MonadWidget t m) => Event t File -> m (Event t String)
+dataURLFileReader :: (MonadWidget t m) => Event t File -> m (Event t Text)
 dataURLFileReader request =
   do fileReader <- liftIO newFileReader
-     let getResultString :: FileReader -> IO (Maybe String)
-         getResultString fr = do v <- getResult fr
-                                 s <- fromJSVal v
-                                 return (fmap fromJSString s)
-         handler :: (Maybe String -> IO ()) -> EventM FileReader UIEvent ()
-         handler k = liftIO $ k =<< getResultString fileReader
      performEvent_ (fmap (\f -> readAsDataURL fileReader (Just f)) request)
-     e <- buildEvent (on fileReader load . handler)
+     e <- wrapDomEvent fileReader (`on` load) . liftIO $ do
+       v <- getResult fileReader
+       s <- fromJSVal v
+       return . fmap T.pack $ s
      return (fmapMaybe id e)
-

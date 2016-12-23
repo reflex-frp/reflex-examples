f66f3ad29720132db93511e6c51334afea261cbd
roland <rsx@bluewin.ch>
2016-12-23 21:07:51 +0100

    Avoid -Wall warnings

--- Keyboard/Keyboard.hs
+++ Keyboard/Keyboard.hs
@@ -10,9 +10,7 @@ import Control.Monad
 import Control.Monad.Trans
 import qualified Data.List.NonEmpty
 import GHCJS.DOM.Element (focus)
-import GHCJS.DOM.HTMLInputElement  hiding (setValue)-- (getSelectionStart,setSelectionStart,setSelectionEnd)
-import GHCJS.DOM.Selection  -- (getSelectionStart,setSelectionStart,setSelectionEnd)
-import GHCJS.DOM.Types (HTMLInputElement)
+import GHCJS.DOM.HTMLInputElement  hiding (setValue)
 import qualified Data.Text as T
 import Data.Monoid
 
@@ -25,16 +23,15 @@ fromListE = fmap Data.List.NonEmpty.head . mergeList
 performArg :: MonadWidget t m => (b -> IO a) -> Event t b -> m (Event t a)
 performArg f x = performEvent (fmap (liftIO . f) x)
 
-
 inputW  :: forall m t .MonadWidget t m => Event t Char -> m ()
 inputW buttonE = do
     rec let newStringE = attachWith (\v (c,n) -> (n + 1,insertAt n c v)) cur posCharE
             cur = current $ value input        -- actual string
-            element = _textInput_element input -- html element
+            html = _textInput_element input    -- html element
         input <- textInput $ def & setValue .~ fmap snd newStringE
-        posCharE :: Event t (Char,Int) <- performArg (\c -> ((,) c) <$> getSelectionStart element) buttonE
-        delay 0.1 (fmap snd posCharE) >>= performArg (\n -> setSelectionStart element (n+ 1) >> setSelectionEnd element (n + 1)) 
-    void $ performArg (const $ focus element) buttonE -- keep the focus right
+        posCharE :: Event t (Char,Int) <- performArg (\c -> ((,) c) <$> getSelectionStart html) buttonE
+        _ <- delay 0.1 (fmap snd posCharE) >>= performArg (\n -> setSelectionStart html (n+ 1) >> setSelectionEnd html (n + 1))
+    void $ performArg (const $ focus html) buttonE -- keep the focus right
 
 keys :: MonadWidget t m => m [Event t Char]
 keys = forM "qwerty" $ \c -> fmap (const c) <$> button [c]

58253582d53324f08297c7ab3fd92417a0fa5cdd
Divam <dfordivam@gmail.com>
2018-11-09 19:33:14 +0900

    init ob project

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ system ? builtins.currentSystem # TODO: Get rid of this system cruft
+, iosSdkVersion ? "10.2"
+}:
+with import ./.obelisk/impl { inherit system iosSdkVersion; };
+project ./. ({ ... }: {
+  android.applicationId = "systems.obsidian.obelisk.examples.minimal";
+  android.displayName = "Obelisk Minimal Example";
+  ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
+  ios.bundleName = "Obelisk Minimal Example";
+})

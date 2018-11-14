693b097ac1f15d0a35ac022b9e591fbcc1386b25
Divam <dfordivam@gmail.com>
2018-11-14 15:59:37 +0900

    NasaPod example fix

--- frontend/src/Frontend/Examples/NasaPod/Main.hs
+++ frontend/src/Frontend/Examples/NasaPod/Main.hs
@@ -7,13 +7,11 @@ module Frontend.Examples.NasaPod.Main where
 
 import           Data.Aeson                  (FromJSON, ToJSON)
 import           Data.Map                    (Map)
-import qualified Data.Map                    as Map
 import           Data.Monoid                 ((<>))
 import qualified Data.Text                   as T
 import           Data.Text (Text)
 import           GHC.Generics                (Generic)
-import           Reflex.Dom                  hiding (mainWidget)
-import           Reflex.Dom.Core             (mainWidget)
+import           Reflex.Dom
 import Control.Monad.Fix (MonadFix)
 
 data Apod =
@@ -29,9 +27,6 @@ data Apod =
 
 instance FromJSON Apod
 instance ToJSON Apod
-
-main :: IO ()
-main = run $ mainWidget app
 
 app
   :: ( DomBuilder t m
@@ -64,32 +59,28 @@ apod
   -> m ()
 apod apiKey = prerender (blank) $ do
   pb :: Event t () <- getPostBuild
-  let defReq = "https://api.nasa.gov/planetary/apod?api_key=" <> apiKey
-      xhrTxt :: Maybe T.Text -> T.Text
-      xhrTxt = maybe defReq (\d -> defReq <> "&date=" <> d)
-      req md = XhrRequest "GET" (xhrTxt md) def
-  rec eRsp :: Event t XhrResponse <- performRequestAsync $ req <$>
-        leftmost [ Nothing <$ pb
-                 , fmap Just eValidDate
-                 ]
-      let eRspApod :: Event t Apod = fmapMaybe decodeXhrResponse eRsp
-      let imgUrl :: Event t T.Text = fmap url eRspApod
-      let srcAttr :: Event t (Map T.Text T.Text) = ffor imgUrl $ \u -> "src" =: u
-      srcAttrDyn :: Dynamic t (Map T.Text T.Text)
-        <- holdDyn mempty $ leftmost [ srcAttr
-                                     , mempty <$ clear''
-                                     ]
-      elDynAttr "img" srcAttrDyn $ return ()
-      -- previous <- button "<<"
-      date <- textInput $ def &
-          attributes .~ constDyn ("placeholder" =: "YYYY-MM-DD")
-      let eValidDate = ffilter ((==10) . T.length) $ updated $ value date
-      hideableButton :: Dynamic t (m (Event t ()))
-        <- return $ ffor srcAttrDyn $ \m -> if Map.null m
-                                               then return never
-                                               else button "Clear"
-      clear  :: Event t (Event t ())    <- dyn hideableButton
-      clear' :: Behavior t (Event t ()) <- hold never clear
-      let clear'' :: Event t () = switch clear'
+  let
+    defReq = "https://api.nasa.gov/planetary/apod?api_key=" <> apiKey
+    xhrTxt :: Maybe T.Text -> T.Text
+    xhrTxt = maybe defReq (\d -> defReq <> "&date=" <> d)
+    req md = XhrRequest "GET" (xhrTxt md) def
+  rec
+    eRsp :: Event t XhrResponse <- performRequestAsync $ req <$>
+      leftmost [ Nothing <$ pb
+               , fmap Just eValidDate
+               ]
+    let
+      eRspApod :: Event t Apod = fmapMaybe decodeXhrResponse eRsp
+      imgUrl :: Event t T.Text = fmap url eRspApod
+      explEv :: Event t T.Text = fmap explanation eRspApod
+      srcAttr :: Event t (Map T.Text T.Text) = ffor imgUrl $ \u -> "src" =: u
+    srcAttrDyn :: Dynamic t (Map T.Text T.Text)
+      <- holdDyn mempty srcAttr
+    elDynAttr "img" srcAttrDyn $ return ()
+    date <- textInput $ def &
+        attributes .~ constDyn ("placeholder" =: "YYYY-MM-DD")
+    let eValidDate = ffilter ((==10) . T.length) $ updated $ value date
+    el "p" $
+      dynText =<< holdDyn "Waiting for response" explEv
   return ()
 

3917cf86f001d09be043052030e0b6c09e9e1420
hsloan <ishaqsloan@gmail.com>
2018-12-03 22:48:39 +0000

    add withHoogle to get local server of modules in scope

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
 { system ? builtins.currentSystem # TODO: Get rid of this system cruft
 , iosSdkVersion ? "10.2"
+, withHoogle ? false
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
 project ./. ({ ... }: {
+  inherit withHoogle;
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";

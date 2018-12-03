3ddd97272400e8310bb7de90bd8d0dd9f024ce93
hsloan <ishaqsloan@gmail.com>
2018-12-03 22:52:42 +0000

    add withHoogle comment

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { system ? builtins.currentSystem # TODO: Get rid of this system cruft
 , iosSdkVersion ? "10.2"
-, withHoogle ? false
+, withHoogle ? false # to spin up localhost:8080 hoogle use: nix-shell --arg withHoogle true -A shells.ghc --command "hoogle server -p 8080 --local"
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
 project ./. ({ ... }: {

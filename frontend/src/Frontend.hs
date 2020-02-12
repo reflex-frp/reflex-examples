49e19b3c16bdd2d07ff780a494fe7cd91a22af10
Matthew Bauer <mjbauer95@gmail.com>
2020-02-12 15:35:29 -0500

    Use proper name for obelisk-executable-config
    Should be obelisk-executable-config-lookup on frontend. Also need to
    use new API.

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -12,9 +12,11 @@ import Obelisk.Frontend
 import Obelisk.Route
 import Obelisk.Route.Frontend
 import Reflex.Dom.Core
-import qualified Obelisk.ExecutableConfig as Cfg
+import qualified Obelisk.Configs as Cfg
 
 import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+import qualified Data.Map as Map
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Fix (MonadFix)
 import Common.Route
@@ -39,7 +41,8 @@ frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
   { _frontend_head = prerender_ pageHead pageHead
   , _frontend_body = do
-      r <- liftIO $ Cfg.get "config/common/route"
+      configs <- Cfg.getConfigs
+      let r = fmap decodeUtf8 $ Map.lookup "config/common/route" configs
       el "header" $ nav
       el "main" $ article $ subRoute_ $ \case
         FrontendRoute_Home -> home
